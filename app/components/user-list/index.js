b70d883a17c00ab219e8e0df9b447777b21c49d9
Karapet Hambardzumyan <karapethambardzumyan97@gmail.com>
2018-11-07 17:03:32 +0400

    Completed

--- app/components/user-list/index.js
+++ app/components/user-list/index.js
@@ -4,6 +4,8 @@
   angular
     .module('app')
     .controller('UserListCtrl', ['$rootScope', '$scope', '$translate', 'SpinnerService', 'UsersService', function($rootScope, $scope, $translate, SpinnerService, UsersService) {
+      $scope.profile = UsersService.profile;
+
       $scope.onMouseOver = function($index) {
         $scope.index = $index;
       };
